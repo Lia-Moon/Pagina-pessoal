88569c3d33418f9010c3109ce7765cd793e54b81
Lia-Moon <juliasoaresvieira@hotmail.com>
2023-05-18 17:47:13 -0300

    Edição das classes e id's para mudar as cores do título

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function myFunction() {
     var element = document.body;
     element.classList.toggle("light__dark__mode");
+
+    //var element = document.getElementById("logo-nav-nome--span");
+    //element.classList.toggle("logo-nav-nome--span");
+    //document.getElementById("logo-nav-nome--span").classList.remove("logo-nav-nome"); -> FUNCIONOU
+    document.getElementById("id__logo-nav-nome").classList = "logo-nav-nome light__dark__mode";
 }
+
+console.log(document.getElementById("id__logo-nav-nome").classList);
