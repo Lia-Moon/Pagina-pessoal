8a2699800cc39c5a5c5f4ac25f8f7b7ac5339a3f
Lia-Moon <juliasoaresvieira@hotmail.com>
2023-05-19 12:03:42 -0300

    Inclusão dos ícones do switcher dark/light mode

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("light__dark__mode");  
 }
+
+const sun = document.querySelector(".light__dark__mode--switch--sun");
+const moon = document.querySelector(".light__dark__mode--switch--moon");
+
+const toggleSwitch = document.querySelector('.botoes__dark__lang--switch input[type="checkbox"]'); // posso usar toggleSwitch.checked depois para verificar se foi selecionado
+
+toggleSwitch.addEventListener("click", () => {
+    if(toggleSwitch.checked) {
+        sun.classList.remove("visible");
+        moon.classList.add("visible");
+    } else {
+        sun.classList.add("visible");
+        moon.classList.remove("visible");
+    }
+})
