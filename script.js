153c26c2b99b0ae26218f1c3ebcd878a7caa577f
jujusvieira <livrosquefazemsonhar@gmail.com>
2023-05-18 22:22:09 -0300

    Melhorias nas cores do dark-mode

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 function myFunction() {
     var element = document.body;
-    element.classList.toggle("light__dark__mode");
-
-    //var element = document.getElementById("logo-nav-nome--span");
-    //element.classList.toggle("logo-nav-nome--span");
-    //document.getElementById("logo-nav-nome--span").classList.remove("logo-nav-nome"); -> FUNCIONOU
-    document.getElementById("id__logo-nav-nome").classList = "logo-nav-nome light__dark__mode";
+    element.classList.toggle("light__dark__mode");  
 }
-
-console.log(document.getElementById("id__logo-nav-nome").classList);
