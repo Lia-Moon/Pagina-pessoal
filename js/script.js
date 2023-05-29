955d80f2c5b8250172bb286a8f70fe7519c4a8f3
jujusvieira <livrosquefazemsonhar@gmail.com>
2023-05-28 22:32:00 -0300

    Adição de botão para tradução inglês-português da página

--- js/script.js
+++ js/script.js
@@ -17,3 +17,49 @@ toggleSwitch.addEventListener("click", () => {
         moon.classList.remove("visible");
     }
 })
+
+// Função relacionada a mudança de linguagem
+window.selectText = function() {
+    var toggleLanguageSwitch = document.getElementById('lang-switch');
+    var lang = toggleLanguageSwitch.checked ? 'en' : 'pt';
+
+    switch (lang) {
+      case 'en':
+        $('[lang]').hide();
+        $('[lang="en"]').show();
+        break;
+      case 'pt':
+        $('[lang]').hide();
+        $('[lang="pt"]').show();
+        break;
+      default:
+        $('[lang]').hide();
+        $('[lang="en"]').show();
+    }
+  };
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var toggleLanguageSwitch = document.getElementById('lang-switch');
+    
+    toggleLanguageSwitch.addEventListener('click', window.selectText);
+    
+    // Disparar o evento para definir o estado inicial do idioma
+    window.selectText();
+  });
+
+
+const portugueseButton = document.querySelector(".language__switch__option.portuguese");
+const englishButton = document.querySelector(".language__switch__option.english");
+
+const toggleLanguageSwitch = document.querySelector('.botoes__dark__lang--switch.language input[type="checkbox"]'); // posso usar toggleSwitch.checked depois para verificar se foi selecionado
+
+
+toggleLanguageSwitch.addEventListener("click", () => {
+  if(toggleLanguageSwitch.checked) {
+      portugueseButton.classList.remove("visible");
+      englishButton.classList.add("visible");
+  } else {
+      portugueseButton.classList.add("visible");
+      englishButton.classList.remove("visible");
+  }
+})
